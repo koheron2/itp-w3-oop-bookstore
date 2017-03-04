23f87406f1fdd5627cdea8fb72b6c5d0423a0516
koheron <koheron2@gmail.com>
2017-03-04 06:57:58 +0000

    Initial Commit

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,55 @@
+#virtualenv = 'KAO_books'
 class Bookstore(object):
-    pass
-
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+    
+    def add_book(self, book):
+        self.books.append(book)
+        
+    def get_books(self):
+        return self.books
+        
+    def search_books(self, title=None, author=None):
+        books_w_query = []
+        if title == None:
+            self.author = author
+            for book in self.books:
+                if self.author == book.author:
+                    books_w_query.append(book)
+        else:
+            self.title = title.lower()
+            for book in self.books:
+                title = book.title
+                lower_title = title.lower()
+                try: 
+                    lower_title.index(self.title)
+                except ValueError:
+                    pass
+                else: 
+                    books_w_query.append(book)
+        return books_w_query
+        
+            
+                    
 
 class Author(object):
-    pass
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.books = []
+        
+    def add_book(self, book):
+        self.books.append(book)
+    
+    def get_books(self):
+        return self.books
 
 
 class Book(object):
-    pass
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.author.add_book(self)
+        
+    
