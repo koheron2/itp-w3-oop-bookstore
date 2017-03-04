07f32dfde8dcb1b4d9b906e1abfa180f2c09e880
koheron <koheron2@gmail.com>
2017-03-04 07:38:34 +0000

    Fixed 'search_books' redundancies and practiced using lambda variables/conditionals as denoted in solution.

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,4 +1,6 @@
 #virtualenv = 'KAO_books'
+_and = lambda x, y: x and y
+_or = lambda x, y: x or y
 class Bookstore(object):
     def __init__(self, name):
         self.name = name
@@ -12,22 +14,20 @@ class Bookstore(object):
         
     def search_books(self, title=None, author=None):
         books_w_query = []
-        if title == None:
-            self.author = author
-            for book in self.books:
-                if self.author == book.author:
-                    books_w_query.append(book)
-        else:
-            self.title = title.lower()
-            for book in self.books:
-                title = book.title
-                lower_title = title.lower()
-                try: 
-                    lower_title.index(self.title)
-                except ValueError:
-                    pass
-                else: 
-                    books_w_query.append(book)
+        
+        for book in self.books:
+            #if title and title.lower() in book.title.lower():
+                #books_w_query.append(book)
+            #if author and author == book.author:
+                #books_w_query.append(book)
+            has_title = title and title.lower() in book.title.lower()
+            has_author = author and author == book.author
+            both_present = title and author
+            operator = _and if both_present else _or
+            condition = operator(has_title, has_author)
+            if condition:
+                books_w_query.append(book)
+            
         return books_w_query
         
             
